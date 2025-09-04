7042b8a2ba065989cdfdb0b3b86fdda1ccee1604
Evgenii <60639601+eliapustin@users.noreply.github.com>
2025-09-04 17:58:45 +0500

    v0.4.0 - [project]

--- src/main.js
+++ src/main.js
@@ -2,21 +2,27 @@ import { app, BrowserWindow, Menu } from 'electron';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
 import {UDPServer} from './udp-server/udp-server'
-import { label } from 'three/tsl';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
   app.quit();
 }
 
+process.env.NODE_ENV = 'development'
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
+
 let udpServer;
 let mainWindow;
+let aboutWindow;
 
-const createWindow = () => {
+function createMainWindow() {
   // Создаем окно браузера
   mainWindow = new BrowserWindow({
+    title: "АТС Симулятор",
     width: 800,
     height: 600,
+    icon: 'src/assets/icon/v2_colour_64x64_8bit.ico',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
@@ -30,7 +36,7 @@ const createWindow = () => {
   }
 
   // Открываем окно разработчика (если требуется).
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
   // Инициализируем UDP сервер после загрузки окна.
   mainWindow.webContents.on('did-finish-load', () => {
@@ -39,21 +45,39 @@ const createWindow = () => {
   })
 };
 
+function createAboutWindow() {
+  // Создаем окно браузера
+  aboutWindow = new BrowserWindow({
+    title: 'О программе АТС Симулятор',
+    width: 600,
+    height: 300,
+    icon: 'src/assets/icon/v2_colour_64x64_8bit.ico',
+    resizable: false,
+    autoHideMenuBar: true,
+  });
+
+  aboutWindow.loadFile('about.html');
+};
+
 // Данный метод будет вызван, когда Electron завершит
 // процесс инициализации и будет готов к созданию окна браузера.
 // Некоторые API могут быть недоступны до этого момента.
 app.whenReady().then(() => {
-  createWindow();
+  createMainWindow();
 
   // Создаем меню
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu)
 
+  // Global shortcuts
+  // globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
+  // globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
+
   // На ОС X этот метод пересоздает окно при нажатии на 
   // иконку в doc и в случае, если нет других открытых окон.
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+      createMainWindow();
     }
   });
 });
@@ -61,51 +85,74 @@ app.whenReady().then(() => {
 
 // Создаем опции для меню
 const menu = [
+  // {role: 'appMenu'}
+  ... (isMac ? [{role: 'appMenu'}] : []),
+  // {role: 'fileMenu'}
+  {
+    label: 'Файл',
+    submenu: [
       {
-        label: 'Файл',
-        submenu: [
-          {
-            label: 'Выйти',
-            click: () => app.quit()
-          }
-        ]
+        label: 'Выйти',
+        accelerator: 'CmdOrCtrl+Q',
+        click: () => app.quit()
+      }
+    ]
+  },
+  ...(isDev ? [
+    {
+      label: 'Разработчик',
+      submenu: [
+        { role: 'reload'},
+        { role: 'forcereload'},
+        { type: 'separator'},
+        { role: 'toggledevtools'},
+      ]
+    }
+  ] : []),
+  // {role: 'modeMenu'}
+  {
+    label: 'Режим',
+    submenu: [
+      {
+        label: 'Обучение',
+        // click: () => app.quit()
       },
       {
-        label: 'Режим',
+        label: 'Полёт',
         submenu: [
           {
-            label: 'Обучение',
-            // click: () => app.quit()
-          },
+            label: 'Авто'
+          },              
           {
-            label: 'Полёт',
-            submenu: [
-              {
-                label: 'Автоматический'
-              },              
-              {
-                label: 'Ручной'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        label: 'Помощь',
-        submenu: [
-          {
-            label: 'О программе'
-            // click: открыть окно с описанием программы
+            label: 'Ручной'
           }
         ]
       }
+    ]
+  },
+  // {role: 'helpMenu'}
+  {
+    label: 'Помощь',
+    submenu: [
+      {
+        label: 'О программе',
+        click: createAboutWindow
+      }
+    ]
+  }
 ]
+
+// для Mac:
+if (isMac) {
+  menu.unshift({role: 'appMenu'})
+}
+
 
 // Выход из приложения при закрытии всех окон. Работает на всех ОС кроме macOS.
 // В macOS, даже при закрытии всех окон приложение все равно остается активным,
 // пока пользователь не нажмет Ctrl + Q
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+  if (!isMac) {
     app.quit();
   }
   if (udpServer) {
